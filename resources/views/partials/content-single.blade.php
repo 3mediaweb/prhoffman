28f95bd13de114e3fdbda39e4464b206b45c0c0c
Adam Chlan <achlan@gmail.com>
2022-05-31 20:52:21 -0400

    blog single

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,19 +1,10 @@
 <article @php(post_class())>
   <header>
-    <h1 class="entry-title">
-      {!! $title !!}
-    </h1>
-
     @include('partials/entry-meta')
   </header>
 
-  <div class="entry-content">
+  <div class="entry-content cb">
     @php(the_content())
   </div>
 
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
-
-  @php(comments_template())
 </article>
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
+@include('partials.page-header')
+
+
 @section('content')
+<div class="max-w-5xl px-5 pt-10 pb-32 mx-auto">
   @while(have_posts()) @php(the_post())
     @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
   @endwhile
+</div>
 @endsection
+
