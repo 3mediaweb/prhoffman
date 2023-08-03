e1958bd1c9bebb05522e3f256c72690d5b2f9bc8
claudia snell <claudiasnell@gmail.com>
2023-08-03 17:47:36 -0400

    adjusting layout of capacity chart page

--- resources/views/partials/content-capacity-chart.blade.php
+++ resources/views/partials/content-capacity-chart.blade.php
@@ -6,6 +6,7 @@
     <h2
     class="text-purple text-2xl md:text-[2.375rem] uppercase font-semibold mb-4 pl-5 md:pl-10 border-l-4 border-coral pt-3">
     Capacity Chart - <?php echo $product_name ?></h2>
-    <div class="pr-5 text-base leading-6 text-gray-600 md:text-lg cb md:max-w-3xl md:pr-0">
+  
+   <div class="relative mx-auto bg-white w-full  p-8 pt-10" style="width: 90%;">
       <?php the_content(); ?>
     </div>
