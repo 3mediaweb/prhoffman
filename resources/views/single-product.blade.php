777b67a2b365ea61a270b9e8ac73acd61fbdf501
claudia snell <claudiasnell@gmail.com>
2023-08-14 17:41:23 -0400

    update main image and add fallback

--- resources/views/single-product.blade.php
+++ resources/views/single-product.blade.php
@@ -2,7 +2,7 @@
 
 <?php $bg = get_field('news_hero_image', 200);
 $banner = get_field('page_banner_image'); ?>
-<section class="bg-center bg-cover min-h-[225px] pb-20" <?php if ($bg) : ?>style="background-image: url(<?php echo $banner; ?>);"<?php endif; ?>>
+<section class="bg-center bg-cover min-h-[225px] pb-20" <?php if ($banner) : ?>style="background-image: url(<?php echo $banner; ?>);"<?php else: ?>style="background-image: url(<?php echo $bg; ?>);" <?php endif; ?>>
   <div class="h-full bg-gradient-to-b from-black">
     <div class="h-full bg-no-repeat bg-contain" style="background-image: url(@asset('assets/images/product-hero-lineart.png')); background-size: 454px 338px;">
       <div class="px-5 <?php if (get_row_index() == 1 ) : ?>pt-32<?php else : ?>py-16<?php endif; ?> mx-auto max-w-7xl">
